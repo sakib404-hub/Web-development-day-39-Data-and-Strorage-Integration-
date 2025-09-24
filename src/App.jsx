455e845b5f5596fd28cf72d3415ff800fb7a3486
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-24 17:42:17 +0600

    created my own user data

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Suspense } from 'react'
+import './App.css'
+import Bottles from './components/bottles/Bottles'
+
+const bottles = async () => {
+  const url = '../public/bottles.json'
+  const res = await fetch(url)
+  return res.json();
+}
+
+function App() {
+  const bottlePromise = bottles();
+  return (
+    <>
+      <h1>Buy Awesome Water Bottles</h1>
+      <Suspense fallback={<p>Bottles Are Loading</p>}>
+        <Bottles bottlePromise={bottlePromise}></Bottles>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
